64908d9c342c4933596c703353c3dd1fad9b160b
Alessandro Scaramuzza <scaramuzzaa@informsrl.it>
2021-03-03 10:13:34 +0100

    uktimo commit

--- ProvaCorsoKennedy/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
+++ ProvaCorsoKennedy/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
@@ -5,7 +5,7 @@ public class ClasseProva {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("ciao a tutti");
+		String s = new String("");
 		
 	}
 	
